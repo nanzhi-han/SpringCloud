2e71b9763098ccf972130563fb0847f47a1f3155
hao <17343004135@163.com>
2019-04-19 09:34:28 +0800

    springBoot项目

--- demo/src/main/java/com/example/WebApplication.java
+++ demo/src/main/java/com/example/WebApplication.java
@@ -14,12 +14,12 @@ import org.springframework.context.annotation.Configuration;
  * @ClassName: WebApplication
  * @Description: 启动类
  */
-//@SpringBootApplication
+@SpringBootApplication
 //添加对 mapper 包扫描@MapperScan
 //@MapperScan("com.example.demo.mapper")
-@Configuration
+/*@Configuration
 @EnableAutoConfiguration
-@EnableAdminServer
+@EnableAdminServer*/
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class,args);
